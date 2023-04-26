20237b0f897569b1da2c72ae1182d5dd82cee420
Emir Tetik <info@frontmir.com>
2023-04-26 18:52:40 +0300

    tailwind eklendi ve module.css ile aynı anda bağlantı sağlandı.

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,23 +1,28 @@
 import React from 'react'
-
+import infocss from "./style/Info.module.css"
  function Info({info,state}) {
   return (
-    <div>
+    <div className=''>
        {
          
 
-  state ?      <section>
-         <div className='info'>
+  state ?      <section className={`grid justify-center  py-64 text-white  ${infocss.info_sec}`}>
+         <div className='info grid gap-4'>
             <p id='city'>{info.name},{info.sys.country}</p>
             
-         </div>
+         <p id='wheather'>{info.weather[0].description}</p>
           <div className='temp_value'>
             <p id='temp'>Temp : {info.main.temp}</p>
           </div>
-          <p id='wheather'>{info.weather[0].description}</p>
           <div className='feel'>
             <p id='feels_like'>Feelslike :{info.main.feels_like}</p>
           </div>
+          <div className=' '>
+          <p id='temp'>Temp : {info.main.temp_max}</p>
+          <p id='temp'>Temp : {info.main.temp_min}</p>
+          </div>
+         </div>
+
      </section> : null
       
        }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,30 +1,36 @@
 import React,{useState} from 'react'
 import axios from "axios"
+import Weatcss from "./style/Weather.module.css"
 function Weather({setInfo,setState,info}) {
 
     const [city, setCity] = useState("");
  
- 
-    const handleChange = async() => {
+    const handleChange  = async (event) => {
+      setCity(event.target.value)
+    }
+
+    const handleSubmit = async() => {
         const api = `b7d530ba0a67a49a6103854563d3f8b8`;
-         const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric&lang=tr`;
+         const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}&units=metric`;
 
       await axios(baseUrl).then(res => setInfo(res.data));
        setState(true);
+    console.log(baseUrl);
+
     } 
   return (
 
-    <div>
-         <section className=''>
-             <h1>Weather</h1>
+    <div >
+         <section  className={` m-0 p-0 antialiased  text-base  items-center py-32 ${Weatcss.weather_sec}`}>
+             <h1 className={`text-3xl font-bold  text-center text-white`}>Weather</h1>
 
-             <form action="" onSubmit={(e) => { e.preventDefault(); handleChange()}}>
+             <form  onSubmit={(e) => { e.preventDefault(); handleSubmit()}} className={`flex justify-center mt-6`}>
   
-               <div className='form'>
-                <input onChange={(e) => setCity(e.target.value)} type="text" placeholder='Search city' className='input' />
+               <div className='form w-52  '>
+                <input onChange={handleChange}  value={city} type="text" placeholder='Search city' className='input h-8 ' />
                </div>
-               <div className='button'>
-                <button type='submit' className='btn'>search</button>
+               <div className='button  bg-slate-600 rounded-full'>
+                <button type='submit' className='btn mt-1 m-2'>search</button>
                </div>
 
              </form>
